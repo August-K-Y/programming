426ed6897a714b5f5bcc35abb48de00ab812c6dd
August-K-Y <kangyan2003@gmail.com>
2017-07-05 16:00:42 -0500

    added TempTest for random test

---
+++ src/kang/interview/programming/array/TempTest.java
@@ -0,0 +1,44 @@
+package kang.interview.programming.array;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TempTest {
+
+	public static void main(String[] args) {
+
+//		String str1 = "the dog saw a cat";
+//		String str2 = "the dog chased the cat";
+//		String str3 = "the cat climbed a tree";
+//		String[] tk1 = str1.split(" ");
+//		String[] tk2 = str2.split(" ");
+//		String[] tk3 = str3.split(" ");
+//		
+//		Set<String> set = new TreeSet<>();
+//		
+//		for(String t : tk1) {
+//			set.add(t);
+//		}
+//		for(String t : tk2) {
+//			set.add(t);
+//		}
+//		for(String t : tk3) {
+//			set.add(t);
+//		}
+//		set.stream().forEach(a -> {System.out.print(a + " ");});
+		
+		
+//		int num = Integer.MAX_VALUE;
+		int num = 1000;
+		int num2 = num >> 2;
+		int num3 = num >>> 1;
+		int num4 = 1 << 0;
+		System.out.println(num);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+	}
+	
+	
+
+}
