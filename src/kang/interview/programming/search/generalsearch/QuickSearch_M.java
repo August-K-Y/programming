5d3ba02d2baf3a56dfd711603f17a048a5cb7825
August-K-Y <kangyan2003@gmail.com>
2017-07-11 19:45:04 -0500

    experiment on quick search

--- src/kang/interview/programming/search/generalsearch/QuickSearch_M.java
+++ src/kang/interview/programming/search/generalsearch/QuickSearch_M.java
@@ -2,7 +2,10 @@ package kang.interview.programming.search.generalsearch;
 
 import java.util.Random;
 
+import kang.interview.programming.util.DataPrinter;
+
 public class QuickSearch_M {
+	
 	private Random rm = new Random();
 	
 	public int search(int[] array, int k) {
@@ -10,7 +13,7 @@ public class QuickSearch_M {
 	}
 	
 	private int findKthLargest(int[] array, int left, int right, int k) {
-		int pivotIndex = partition(array, left, right);
+		int pivotIndex = partition_(array, left, right);
 		int rank = pivotIndex + 1;
 		if (rank == k) {
 			return array[pivotIndex];
@@ -21,6 +24,20 @@ public class QuickSearch_M {
 		}
 	}
 
+	/**
+	 * This is the core function of the quick search algorithm. It partitions
+	 * the sub-array of the specified array into two parts.  from index of left to index of right
+	 * 
+	 * @param array
+	 *            the array of which a sub-array is to be partitioned upon
+	 * @param left
+	 *            the left boundary index of the sub-array to be partitioned
+	 *            upon
+	 * @param right
+	 *            the right boundary index of the sub-array to be partitioned
+	 *            upon
+	 * @return the index of pivot in the array
+	 */
 	private int partition(int[] array, int left, int right) {
 		
 		int pivot = array[rm.nextInt(right - left + 1) + left];
@@ -32,6 +49,32 @@ public class QuickSearch_M {
 		}
 		return left;
 	}
+	
+	private int partition_(int[] array, int left, int right) {
+		
+		int pivotIndex = rm.nextInt(right - left + 1) + left;
+		int pivot = array[pivotIndex];
+		int temp = left;
+		swap(array, left++, pivotIndex);
+		while(left < right) {
+			while (left < right && array[left] <= pivot) left++;
+			while(left < right && array[right] >= pivot) right--;
+			if (left < right) {
+				swap(array, left, right);
+				right--;
+				left++;
+			}
+		}
+		swap(array, temp, right);
+		
+		System.out.println("pivotIndex:" + pivotIndex);
+		System.out.println("temp:" + temp);
+		System.out.println("pivot:" + pivot);
+		System.out.println("right: " + right);
+		DataPrinter.printArray(array);
+		
+		return right;
+	}
 
 	private void swap(int[] array, int left, int right) {
 		int temp = array[left];
@@ -42,8 +85,10 @@ public class QuickSearch_M {
 	public static void main(String[] args) {
 		QuickSearch_M s = new QuickSearch_M();
 		int[] board = { 3, 2, 1, 5, 4 };
-		System.out.println(s.search(board, 1));
-		System.out.println(s.search(board, 3));
-		System.out.println(s.search(board, 5));
+//		int[] board = { 3, 2, 1, 2,2,2,5, 4 };
+//		System.out.println(s.search(board, 1));
+//		System.out.println(s.search(board, 3));
+//		System.out.println(s.search(board, 5));
+		s.partition_(board, 0, board.length - 1);
 	}
 }
