9767e4fc2b85b026b661f39d4b270bea9d0a4c40
August-K-Y <kangyan2003@gmail.com>
2017-07-20 01:22:57 -0500

    added shell for ThreeSum

--- src/kang/interview/programming/array/ThreeSum.java
+++ src/kang/interview/programming/array/ThreeSum.java
@@ -1,61 +1,24 @@
 package kang.interview.programming.array;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
+/**
+ * LeetCode 15. 3Sum: https://leetcode.com/problems/3sum/#/description
+ * 
+ * Given an array S of n integers, are there elements a, b, c in S such that a +
+ * b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+ * 
+ * Note: The solution set must not contain duplicate triplets.
+ * 
+ * For example, given array S = [-1, 0, 1, 2, -1, -4],
+ * 
+ * A solution set is: [ [-1, 0, 1], [-1, -1, 2] ]
+ * 
+ * @author yankang
+ *
+ */
 public class ThreeSum {
-
-	/**
-	 * O(n*nlogn)
-	 * @param input
-	 * @param value
-	 * @return
-	 */
-	public boolean hasThreeSum_unsorted_duplicateAllowed(List<Integer> input, int value) {
-		List<Integer> copiedInput = new ArrayList<>(input);
-		Collections.sort(copiedInput);
-
-		for (int e : input) {
-			return hasTwoSum(copiedInput, value - e);
-		}
-		return false;
+	public List<List<Integer>> threeSum(int[] nums) {
+		return null;
 	}
-	
-	public boolean hasThreeSum_unsorted_duplicateNotAllowed(List<Integer> input, int value) {
-		// TODO:
-		return false;
-	}
-
-	private boolean hasTwoSum(List<Integer> copiedInput, int value) {
-		int left = 0;
-		int right = copiedInput.size() - 1;
-		while (left <= right) {
-			int sum = copiedInput.get(left) + copiedInput.get(right);
-			if (sum == value) {
-				return true;
-			} else if (sum < value) {
-				left++;
-			} else {
-				right--;
-			}
-		}
-		return false;
-	}
-
-	public static void main(String[] args) {
-		List<Integer> list = new ArrayList<>();
-		list.add(11);
-		list.add(2);
-		list.add(5);
-		list.add(7);
-		list.add(3);
-
-		ThreeSum t = new ThreeSum();
-		System.out.println(t.hasThreeSum_unsorted_duplicateAllowed(list, 21));
-		System.out.println(t.hasThreeSum_unsorted_duplicateAllowed(list, 22));
-	}
-
 }
